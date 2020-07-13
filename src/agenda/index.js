55a84ffb57720d05d43a852bfbffe653385c7d8b
Luc Iyer <luc@mbp.local>
2020-07-13 06:54:18 -0300

    update

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -3,8 +3,8 @@ require("dotenv").config()
 const Agenda = require("agenda")
 const fetch = require("node-fetch")
 
-const twitter = require(appRoot + "/src/tweet")
 const card = require(appRoot + "/src/card")
+const twitter = require(appRoot + "/src/tweet")
 
 const db_uri = process.env.MONGODB_URI || "mongodb://localhost/dev"
 
@@ -35,9 +35,8 @@ queue.define("send repetition", async job => {
 
   const { card_id, to_user, message } = job.attrs.data
   const send_result = await twitter.newThread(to_user, message)
+  const created_rep = await card.createRep(card_id, send_result.id_str)
 
-  await card.createRep(card_id, send_result.id_str)
-  
 })
 
 module.exports = queue
