f7a302f09e827fc14201d6f969b30a2533ef24d6
Luc Iyer <luc@mbp.local>
2020-07-13 03:46:28 -0300

    startup messages __good__

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,9 @@ const agenda = require(appRoot + "/src/agenda")
 const serverUp = async () => {
   console.log("Server running.")
   await agenda.start()
+  console.log("Job queue initialized.")
   agenda.every("25 minutes", "keep server awake")
+  console.log("Server will be kept awake.")
 }
 
 module.exports = {
